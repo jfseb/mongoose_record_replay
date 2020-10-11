e1ab4a16879f2ec676988fcf1ec1fa0849f1650e
jfseb <jfsebpublic@gmail.com>
2020-10-11 02:04:05 +0200

    0.12

--- js/mgrecrep.d.ts
+++ js/mgrecrep.d.ts
@@ -1,5 +1,5 @@
 /**
- * instrument mongoose to record/replay queries ( !! only queries so far)
+ * instrument mongoose to record/replay queries (!! only queries so far)
  *
  * allows to run (mongoose read only) unit tests w.o. a mongoose instance
  *
@@ -8,14 +8,18 @@
 /// <reference types="node" />
 import * as mongoose from 'mongoose';
 import * as events from 'events';
+/**
+ * The recording path, set via argument
+ * or
+ */
 export declare function JSONParse(text: string): any;
 export declare function JSONStringify(obj: any): string;
-export declare function instrumentModel(model: mongoose.Model<any>): any;
+export declare function instrumentModel(model: mongoose.Model<any>, recordingPath: string, theMode: string): any;
 export declare function digestArgs(op: string, name: string, query: any): string;
-export declare function recordOp(op: string, name: string, query: any, res: any): void;
-export declare function retrieveOp(op: string, name: string, query: any): any;
-export declare function instrumentModelRecord(modelDoc: mongoose.Model<any>): void;
-export declare function instrumentModelReplay(modelDoc: mongoose.Model<any>): void;
+export declare function recordOp(op: string, name: string, query: any, res: any, recordingPath: string): void;
+export declare function retrieveOp(op: string, name: string, query: any, recordingPath: string): any;
+export declare function instrumentModelRecord(modelDoc: mongoose.Model<any>, recordingPath: string, theMode: string): void;
+export declare function instrumentModelReplay(modelDoc: mongoose.Model<any>, recordingPath: string): void;
 /**
  * funtion to instrument mongoose
  *
@@ -25,13 +29,16 @@ export declare function instrumentModelReplay(modelDoc: mongoose.Model<any>): vo
  * @param [path] {string} optional, a path to write/read files from, defaults to "mgrecrep/"
  * @param mode {string}  undefined (environment value) or "REPLAY" or "RECORD"
  */
-export declare function instrumentMongoose(mongoose: mongoose.Mongoose, path?: string, mode?: string): mongoose.Mongoose;
+export declare function instrumentMongoose(mongoose: mongoose.Mongoose, path: string, mode?: string): mongoose.Mongoose;
 export declare var mongooseMock: {
     models: {};
+    recordingPath: string;
+    theMode: string;
     modelNames: () => string[];
     Schema: any;
     model: (a: any, b: any) => any;
     disconnect: () => void;
     connect: (connStr: string) => void;
+    set: (a: any, b: any) => void;
     connection: events.EventEmitter;
 };
--- src/mgrecrep.ts
+++ src/mgrecrep.ts
@@ -1,5 +1,5 @@
 /**
- * instrument mongoose to record/replay queries ( !! only queries so far)
+ * instrument mongoose to record/replay queries (!! only queries so far)
  *
  * allows to run (mongoose read only) unit tests w.o. a mongoose instance
  *
@@ -9,7 +9,6 @@
 import * as debugf from 'debugf';
 
 var debuglog = debugf('mongoose_record_replay');
-//const loadlog = logger.logger('modelload', '');
 
 const path = require('path');
 import * as process from 'process';
@@ -18,15 +17,10 @@ import * as events from 'events';
 import * as fs from 'fs';
 const crypto = require('crypto');
 
-
 /**
  * The recording path, set via argument
  * or
  */
-var recordingPath = 'mongoose_record_replay/';
-
-var theMode = undefined;
-
 export function JSONParse(text: string): any {
     function customDeSer(key, value) {
         if (value.toString().indexOf("__REGEXP ") == 0) {
@@ -37,8 +31,6 @@ export function JSONParse(text: string): any {
     }
     return JSON.parse(text, customDeSer);
 }
-
-
 
 export function JSONStringify(obj: any): string {
     function customSer(key, value) {
@@ -52,12 +44,12 @@ export function JSONStringify(obj: any): string {
 }
 
 function readFileAsJSON(filename: string): any {
-    var data = fs.readFileSync(filename, 'utf-8');
     try {
+        var data = fs.readFileSync(filename, 'utf-8');
         return JSON.parse(data);
     } catch (e) {
         console.log("Content of file " + filename + " is no json" + e);
-        process.exit(-1);
+        throw new Error("Content of file " + filename + " is no json" + e);
     }
     return undefined;
 }
@@ -80,18 +72,18 @@ var dbEmitter = new events.EventEmitter();
 // unit test invoke this multiple times, avoid node js warning
 dbEmitter.setMaxListeners(0);
 
-export function instrumentModel(model: mongoose.Model<any>) {
+export function instrumentModel(model: mongoose.Model<any>, recordingPath : string, theMode: string) {
     if (theMode === "RECORD") {
-        instrumentModelRecord(model);
+        instrumentModelRecord(model, recordingPath, theMode);
     } else if (theMode === "REPLAY") {
         // todo
-        instrumentModelReplay(model);
+        instrumentModelReplay(model, recordingPath);
     }
     return model;
 }
 
 
-function makeFileName(digest) {
+function makeFileName(digest, recordingPath: string) {
     return (recordingPath + 'data/' + digest + '.json');
 }
 
@@ -103,7 +95,7 @@ export function digestArgs(op: string, name : string, query : any) {
     return digest;
 }
 
-export function recordOp(op: string, name: string, query: any, res: any) {
+export function recordOp(op: string, name: string, query: any, res: any, recordingPath : string) {
     var digest = digestArgs(op,name,query);
     var resStr = JSON.stringify(res, undefined, 2);
     var len = 0;
@@ -112,7 +104,7 @@ export function recordOp(op: string, name: string, query: any, res: any) {
     } else {
         len = resStr.length;
     }
-    var filename = makeFileName(digest);
+    var filename = makeFileName(digest, recordingPath);
     console.log( 'recording to file: ' + filename + ' (' + path.normalize(filename) + ')...');
     fs.writeFileSync(filename, resStr);
     var known = {};
@@ -131,9 +123,9 @@ export function recordOp(op: string, name: string, query: any, res: any) {
     fs.writeFileSync(recordingPath + 'queries.json', JSONStringify(known));
 }
 
-export function retrieveOp(op: string, name: string, query: any) {
+export function retrieveOp(op: string, name: string, query: any, recordingPath : string) {
     var digest = digestArgs(op,name, query);
-    var filename = makeFileName(digest);
+    var filename = makeFileName(digest, recordingPath);
     debuglog(' reading from filename ' + filename);
     try {
         var res = readFileAsJSON(filename);
@@ -149,8 +141,8 @@ export function retrieveOp(op: string, name: string, query: any) {
     return res;
 }
 
-export function instrumentModelRecord(modelDoc: mongoose.Model<any>) {
-    console.log('mongoose_record_replay is instrumenting model ' + modelDoc.modelName + ' for recording to ' + recordingPath );
+export function instrumentModelRecord(modelDoc: mongoose.Model<any>, recordingPath: string, theMode: string) {
+    console.log('***mongoose_record_replay is instrumenting model ' + modelDoc.modelName + ' for recording to ' + recordingPath );
     var oFind = modelDoc.find;
     modelDoc.find = function (): any {
         debuglog('someone is calling find with ' + modelDoc.modelName + JSON.stringify(arguments, undefined, 2));
@@ -161,7 +153,7 @@ export function instrumentModelRecord(modelDoc: mongoose.Model<any>) {
         var query = arguments[0];
         res.lean().exec().then((a) => {
             //console.log("here result1 + " + JSON.stringify(a, undefined,2) );
-            recordOp("find", modelDoc.modelName, query, a);
+            recordOp("find", modelDoc.modelName, query, a, recordingPath);
         }
         );
         return res;
@@ -177,7 +169,7 @@ export function instrumentModelRecord(modelDoc: mongoose.Model<any>) {
         var res2 = res.then((a) => {
             debuglog( () => "here result1 + " + JSON.stringify(a, undefined,2) );
             try {
-                recordOp("distinct", modelDoc.modelName, query, a);
+                recordOp("distinct", modelDoc.modelName, query, a, recordingPath);
             } catch( ex)
             {
                 console.log( ' recording to file failed ' + ex );
@@ -196,21 +188,21 @@ export function instrumentModelRecord(modelDoc: mongoose.Model<any>) {
         var res = oAggregate.apply(modelDoc, arguments);
         res.then((a) => {
             debuglog(() => "here result1 + " + JSON.stringify(a, undefined, 2));
-            recordOp("aggregate", modelDoc.modelName, query, a);
+            recordOp("aggregate", modelDoc.modelName, query, a, recordingPath);
         }
         );
         return res;
     }
 }
 
-export function instrumentModelReplay(modelDoc: mongoose.Model<any>) {
+export function instrumentModelReplay(modelDoc: mongoose.Model<any>, recordingPath: string) {
     console.log('instrumenting model ' + modelDoc.modelName + ' for replay from path ' + recordingPath );
     debuglog('instrumenting model ' + modelDoc.modelName);
     var oFind = modelDoc.find;
     modelDoc.find = function (): any {
         debuglog(() => 'someone is replaying find with' + JSON.stringify(arguments, undefined, 2));
         var query = arguments[0];
-        var res = retrieveOp("find", modelDoc.modelName, query);
+        var res = retrieveOp("find", modelDoc.modelName, query, recordingPath);
         debuglog(() => 'returning res ' + JSON.stringify(res) + ' for query find' + query);
         return {
             lean: function () {
@@ -230,7 +222,7 @@ export function instrumentModelReplay(modelDoc: mongoose.Model<any>) {
     modelDoc.distinct = function (): any {
         debuglog('someone is replaying distinct with' + JSON.stringify(arguments, undefined, 2));
         var query = arguments[0];
-        var res = retrieveOp("distinct", modelDoc.modelName, query);
+        var res = retrieveOp("distinct", modelDoc.modelName, query, recordingPath);
         debuglog('returning res ' + JSON.stringify(res) + ' for query find' + query);
         return new Promise(function (resolve, reject) {
             setTimeout(function () { resolve(res); }, 0);
@@ -240,7 +232,7 @@ export function instrumentModelReplay(modelDoc: mongoose.Model<any>) {
     modelDoc.aggregate = function (): any {
         debuglog('someone is replaying aggregate with' + JSON.stringify(arguments, undefined, 2));
         var query = Array.prototype.slice.call(arguments);
-        var res = retrieveOp("aggregate", modelDoc.modelName, query);
+        var res = retrieveOp("aggregate", modelDoc.modelName, query, recordingPath);
         var p = new Promise(function (resolve, reject) {
             setTimeout(function () { resolve(res); }, 0);
         });
@@ -260,35 +252,87 @@ export function instrumentModelReplay(modelDoc: mongoose.Model<any>) {
  * @param [path] {string} optional, a path to write/read files from, defaults to "mgrecrep/"
  * @param mode {string}  undefined (environment value) or "REPLAY" or "RECORD"
  */
-export function instrumentMongoose(mongoose: mongoose.Mongoose, path?: string, mode?: string): mongoose.Mongoose {
-    theMode = mode || process.env.MONGO_RECORD_REPLAY;
+export function instrumentMongoose(mongoose: mongoose.Mongoose, path: string, mode?: string): mongoose.Mongoose {
+    console.log(' ********* instrument mongoose with  ' + path + "  " + mode);
+    var theMode = mode || process.env.MONGO_RECORD_REPLAY;
     if (theMode && ["REPLAY", "RECORD"].indexOf(mode) < 0) {
         console.log('passed mode value or env MONGO_RECORD_REPLAY may only be "RECORD" or "REPLAY" , MONGO_RECORD MONGO_REPLAY');
         throw new Error('mongoose_record_replay mode should be one of "REPLAY", "RECORD"  was ' + theMode);
     }
     if (theMode === "RECORD") {
-        recordingPath = path || process.env.MONGO_RECORD_REPLAY_PATH || "mongoose_record_replay";
-        console.log( '!* mode RECORD to path ' + recordingPath );
+        var recordingPath = path || process.env.MONGO_RECORD_REPLAY_PATH || "mongoose_record_replay";
+        console.log( '!* mode RECORD to path ' + recordingPath  + ' in ' + __dirname + " "  + mode );
         assurePath(recordingPath);
         var omodel = mongoose.model;
         mongoose.model = function () {
             if (arguments.length > 1) {
-                return instrumentModel(omodel.apply(mongoose, arguments));
+                return instrumentModel(omodel.apply(mongoose, arguments),recordingPath,theMode);
             }
             return omodel.apply(mongoose, arguments);
         }
         return mongoose;
     } else if (theMode === "REPLAY") {
-        console.log( '!* mode REPLAY from path ' + recordingPath );
         recordingPath = path || process.env.MONGO_RECORD_REPLAY_PATH || "mongoose_record_replay";
-        return mongooseMock as any;
+        console.log( '!* mode REPLAY from path ' + recordingPath  + ' in ' + __dirname + " "  + mode  + " " + path);
+    //    var r = mongooseMock;
+        var r = makeMongooseMock(recordingPath,theMode);
+        return r; 
     }
     return mongoose;
 }
 
+var mocksPerPath = {};
+
+function makeMongooseMock(recordingPath: string, theMode: string) {
+    if ( mocksPerPath[recordingPath] == undefined) {
+        var res = {
+            models: {},
+            recordingPath : recordingPath,
+            theMode : theMode,
+            modelNames: function () {
+                return Object.keys(this.models);
+            },
+            Schema: mongoose.Schema,
+        
+            model: function (a, b) {
+                if (b === undefined) {
+                    return this.models[a];
+                }
+                debuglog('creating model  ' + a + ' at mock');
+                this.models[a] = instrumentModel({
+                    find: function () { },
+                    aggregate: function () { },
+                    distinct: function () { },
+                    modelName: a,
+                    schema: b,
+                } as any, this.recordingPath, this.theMode);
+                return this.models[a];
+            },
+            disconnect: function () {
+                debuglog('simulationg disconnect ');
+            },
+            connect: function (connStr: string) {
+                // this.db.on.emit('on');
+                debuglog('simulationg connecting to ' + connStr);
+                if (!this._once) {
+                    var that = this;
+                    setTimeout(function () {
+                        that.connection.emit('open');
+                        debuglog('fired emit');
+                    }, 0);
+                }
+            },
+            connection: dbEmitter
+        };
+        mocksPerPath[recordingPath] = res;
+    }
+    return mocksPerPath[recordingPath];
+}
 
 export var mongooseMock = {
     models: {},
+    recordingPath : "",
+    theMode : "",
     modelNames: function () {
         return Object.keys(this.models);
     },
@@ -305,7 +349,7 @@ export var mongooseMock = {
             distinct: function () { },
             modelName: a,
             schema: b,
-        } as any);
+        } as any, this.recordingPath, this.theMode);
         return this.models[a];
     },
     disconnect: function () {
@@ -322,5 +366,7 @@ export var mongooseMock = {
             }, 0);
         }
     },
+    // e.g. set('useCreateIndex',true)
+    set : function(a,b) {},
     connection: dbEmitter
 };
